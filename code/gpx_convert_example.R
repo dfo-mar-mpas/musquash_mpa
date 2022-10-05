a846efd37eee25a5037b2369d61dc606b7c9bc53
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2022-10-05 15:25:50 -0300

    updated filepath

--- code/gpx_convert_example.R
+++ code/gpx_convert_example.R
@@ -4,11 +4,11 @@ library(sf)
 library(dplyr)
 
 #read in the coordinates and convert them to an sf then convert that sf into an 'sp' datatype
-test <- read.csv("c:/Users/stanleyr/Documents/Github/musquash_mpa/data/eDNA_Sample_Stations_2022.csv")%>%
+test <- read.csv("data/eDNA_Sample_Stations_2022.csv")%>%
         st_as_sf(coords=c("long","lat"),crs=latlong)%>%
         as_Spatial()
 
 #write the sp datatype to a gpx using the rgdal::writeOGR
 writeOGR(test,
-         dsn="c:/Users/stanleyr/Documents/Github/musquash_mpa/data/musq_stations.gpx", layer="name", driver="GPX",
+         dsn="data/musq_stations.gpx", layer="name", driver="GPX",
          dataset_options="GPX_USE_EXTENSIONS=yes", overwrite_layer = T)
